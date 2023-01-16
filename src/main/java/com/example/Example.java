7f1d6c26a875c93831f2a275c2b14f9cfbb54acd
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-01-16 09:12:57 +0100

    Disable `Varifier` and improve examples

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -1,23 +1,21 @@
 package com.example;
 
+import static java.util.Objects.requireNonNullElse;
+
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.math.BigDecimal;
-import java.util.Map;
 
 public final class Example {
   private Example() {}
-
-  static final int number = 5;
-
-  public static void main(String[] args) {}
 
   static BigDecimal getNumber() {
     return BigDecimal.valueOf(0);
   }
 
   public ImmutableSet<Integer> getSet() {
-    Map<Integer, Integer> map = Map.of(1, 2);
-    Integer integer = map.getOrDefault(1, 3);
+    ImmutableMap<Integer, Integer> map = ImmutableMap.of(1, 2);
+    requireNonNullElse(map.get(1), 2);
 
     ImmutableSet<Integer> set = ImmutableSet.of(1);
     return ImmutableSet.copyOf(set);
