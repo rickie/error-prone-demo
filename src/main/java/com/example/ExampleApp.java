93d57f2cdb6438a072deb53a39caefd0881f33bc
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-01-30 21:00:44 +0100

    Move files to `other` package

--- src/main/java/com/example/ExampleApp.java
+++ src/main/java/com/example/ExampleApp.java
@@ -5,13 +5,13 @@ import com.google.common.collect.ImmutableSet;
 public final class ExampleApp {
   public static void main(String[] args) {
     if (args.length != 0) {
-      new IllegalArgumentException();
+      throw new IllegalArgumentException();
     }
     System.out.println("Hello world!");
   }
 
   public ImmutableSet<Integer> foo() {
     ImmutableSet<Integer> set = ImmutableSet.of(1);
-    return ImmutableSet.copyOf(set);
+    return set;
   }
 }
