c1905844245392241fd607479d70e977826008d9
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-01-31 19:08:29 +0100

    Set back default

--- src/main/java/com/example/ExampleApp.java
+++ src/main/java/com/example/ExampleApp.java
@@ -5,13 +5,13 @@ import com.google.common.collect.ImmutableSet;
 public final class ExampleApp {
   public static void main(String[] args) {
     if (args.length != 0) {
-      throw new IllegalArgumentException();
+      new IllegalArgumentException();
     }
     System.out.println("Hello world!");
   }
 
   public ImmutableSet<Integer> foo() {
     ImmutableSet<Integer> set = ImmutableSet.of(1);
-    return set;
+    return ImmutableSet.copyOf(set);
   }
 }
